573128e70eb89edca775a35acb725c8ea25b40b6
Hamed <gitlab.mra.sh@gmail.com>
2019-09-03 19:18:21 +0430

    mra-sh/ 2 shahivar

---
+++ assets/js/theme.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltipster({
+        animation: 'grow',
+        delay: 300,
+        theme: 'tooltipster-borderless',
+        trigger: 'hover'
+    });
+});
